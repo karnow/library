33f66f717f0a6b3298db65130dd94f6ed7b2fd7c
carl <karol-nowakowski@o2.pl>
2020-12-27 13:10:45 +0100

    Create graphql server

---
+++ index.js
@@ -0,0 +1,12 @@
+const {ApolloServer} = require("apollo-server");
+const typeDefs = require("./typeDefs");
+const rootValue = require("./rootValue");
+
+const PORT = process.env.PORT || 4000
+
+const server = new ApolloServer({typeDefs, rootValue, playground:true, introspection:true});
+
+server.listen({port: PORT}).then((result) => console.log(result.url, result.port));
+
+
+
