e4dd8cc9f5031af5f4b8de8a4469296c80c98992
carl <karol-nowakowski@o2.pl>
2021-03-16 22:51:32 +0100

    moving the search object to dataAccess

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const resolvers = require("./resolvers");
 const initialData = require("./config/initialData");
 const createDb = require("./data/db");
 const createDataAccess = require("./dataAccess");
-const searchFieldsByType = require("./config/searchFieldsByType");
-const { Search } = require("./data/search");
+
 
 
 const db = createDb(initialData);
 const dataAccess = createDataAccess(db);
-const search = new Search(db, searchFieldsByType);
+
 
 const PORT = process.env.PORT || 4000
 const BASE_ASSETS_URL = process.env.BASE_ASSETS_URL || "http://examples.devmastery.pl/assets";
@@ -20,8 +19,7 @@ const server = new ApolloServer({
      resolvers,
       context: {
           dataAccess,
-          currentUserDbId: "2",
-          search,
+          currentUserDbId: "2",          
           assetsBaseUrl: BASE_ASSETS_URL
       },
        playground:true,
